68634b05e50e62352750b6b44cc2871bbd8451ca
Hoang Si Van <hoangsv@gemvietnam.com>
2018-05-14 17:00:15 +0700

    first

---
+++ Source/Charts/Custom/CustomXAxisRenderer.swift
@@ -0,0 +1,129 @@
+//
+//  CustomXAxisRenderer.swift
+//  Charts
+//
+//  Created by GEM on 5/14/18.
+//
+
+import Foundation
+import CoreGraphics
+
+#if !os(OSX)
+    import UIKit
+#endif
+
+@objc(CustomChartXAxisRenderer)
+open class CustomXAxisRenderer: XAxisRenderer
+{
+    let haveCommentList = [true, false, false, true, true, true]
+    
+    /// draws the x-labels on the specified y-position
+    open override func drawLabels(context: CGContext, pos: CGFloat, anchor: CGPoint)
+    {
+        guard
+            let xAxis = self.axis as? XAxis,
+            let viewPortHandler = self.viewPortHandler,
+            let transformer = self.transformer
+            else { return }
+        
+        #if os(OSX)
+            let paraStyle = NSParagraphStyle.default().mutableCopy() as! NSMutableParagraphStyle
+        #else
+            let paraStyle = NSParagraphStyle.default.mutableCopy() as! NSMutableParagraphStyle
+        #endif
+        paraStyle.alignment = .center
+        
+        //label attrs moved from here
+        //        let labelAttrs = [NSFontAttributeName: xAxis.labelFont,
+        //            NSForegroundColorAttributeName: xAxis.labelTextColor,
+        //            NSParagraphStyleAttributeName: paraStyle] as [String : NSObject]
+        let labelRotationAngleRadians = xAxis.labelRotationAngle * ChartUtils.Math.FDEG2RAD
+        
+        let centeringEnabled = xAxis.isCenterAxisLabelsEnabled
+        
+        let valueToPixelMatrix = transformer.valueToPixelMatrix
+        
+        var position = CGPoint(x: 0.0, y: 0.0)
+        
+        var labelMaxSize = CGSize()
+        
+        if xAxis.isWordWrapEnabled
+        {
+            labelMaxSize.width = xAxis.wordWrapWidthPercent * valueToPixelMatrix.a
+        }
+        
+        let entries = xAxis.entries
+        
+        for i in stride(from: 0, to: entries.count, by: 1)
+        {
+            //label attrs moved to here
+            let labelAttrs: [String: NSObject]!
+            
+            if(i<haveCommentList.count) {
+                if haveCommentList[i] {
+                    labelAttrs = [NSFontAttributeName: xAxis.labelFont,
+                                  NSForegroundColorAttributeName: UIColor.blue,
+                                  NSParagraphStyleAttributeName: paraStyle] as [String : NSObject]
+                } else {
+                    labelAttrs = [NSFontAttributeName: xAxis.labelFont,
+                                  NSForegroundColorAttributeName: xAxis.labelTextColor,
+                                  NSParagraphStyleAttributeName: paraStyle] as [String : NSObject]
+                }
+            }
+            else {
+                labelAttrs = [NSFontAttributeName: xAxis.labelFont,
+                              NSForegroundColorAttributeName: xAxis.labelTextColor,
+                              NSParagraphStyleAttributeName: paraStyle] as [String : NSObject]
+                
+            }
+            
+            if centeringEnabled
+            {
+                position.x = CGFloat(xAxis.centeredEntries[i])
+            }
+            else
+            {
+                position.x = CGFloat(entries[i])
+            }
+            
+            position.y = 0.0
+            position = position.applying(valueToPixelMatrix)
+            
+            if viewPortHandler.isInBoundsX(position.x)
+            {
+                let label = xAxis.valueFormatter?.stringForValue(xAxis.entries[i], axis: xAxis) ?? ""
+                
+                let labelns = label as NSString
+                
+                if xAxis.isAvoidFirstLastClippingEnabled
+                {
+                    // avoid clipping of the last
+                    if i == xAxis.entryCount - 1 && xAxis.entryCount > 1
+                    {
+                        let width = labelns.boundingRect(with: labelMaxSize, options: .usesLineFragmentOrigin, attributes: labelAttrs, context: nil).size.width
+                        
+                        if width > viewPortHandler.offsetRight * 2.0
+                            && position.x + width > viewPortHandler.chartWidth
+                        {
+                            position.x -= width / 2.0
+                        }
+                    }
+                    else if i == 0
+                    { // avoid clipping of the first
+                        let width = labelns.boundingRect(with: labelMaxSize, options: .usesLineFragmentOrigin, attributes: labelAttrs, context: nil).size.width
+                        position.x += width / 2.0
+                    }
+                }
+                
+                drawLabel(context: context,
+                          formattedLabel: label,
+                          x: position.x,
+                          y: pos,
+                          attributes: labelAttrs,
+                          constrainedToSize: labelMaxSize,
+                          anchor: anchor,
+                          angleRadians: labelRotationAngleRadians)
+            }
+        }
+    }
+}
